d6d1ee3d209cd834f0da44ba1122e48649f77b2b
Siddharth Mohril <68049128+siddharth-mohril@users.noreply.github.com>
2026-07-23 12:40:54 +0530

    Time: 23 ms (55.46%) | Memory: 25.1 MB (89.54%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,17 +4,15 @@ class Solution:
         res = [1] * n
         print(res)
         
-        # Pass 1: Calculate prefix products (left-to-right)
+
         prefix = 1
         for i in range(n):
             res[i] = prefix
             prefix *= nums[i]
 
-            
-        # Pass 2: Multiply by suffix products (right-to-left)
+
         postfix = 1
         for i in range(n - 1, -1, -1):
-            res[i] *= postfix  # Multiply current result by the postfix product
-            postfix *= nums[i] # Update postfix product for the next element
-            
+            res[i] *= postfix  
+            postfix *= nums[i] 
         return res
