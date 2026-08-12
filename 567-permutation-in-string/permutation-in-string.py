6f97c7e14a49989fe0d86d3c6e80f9706ac9a5dd
Siddharth Mohril <68049128+siddharth-mohril@users.noreply.github.com>
2026-08-12 10:34:28 -0700

    Time: 16 ms (66.93%) | Memory: 19.4 MB (18.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,37 +1,32 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-        hashmap1 = {}
-        hashmap2 = {}
-
-        L = 0
-
-        if len(s1)> len(s2):
+        if len(s1) > len(s2):
             return False
-
-        for R in range(len(s1)):
-            hashmap1[s1[R]] = hashmap1.get(s1[R],0) + 1
-            hashmap2[s2[R]] = hashmap2.get(s2[R],0) + 1
-
-        if hashmap1 == hashmap2:
-            return True
+        cs1 = {} #counts1 count hashmap for s1
+        for i in range(len(s1)):
+            cs1[s1[i]] = cs1.get(s1[i], 0) + 1
         
-        for R in range(len(s1),len(s2)):
-            hashmap2[s2[R]] = hashmap2.get(s2[R],0) + 1
+        L = 0
+        cs2 = {}
+        for R in range(len(s2)):
 
-            hashmap2[s2[L]] -= 1
-            if hashmap2[s2[L]] == 0 :
-                del hashmap2[s2[L]]
-            L += 1
+            cs2[s2[R]] = cs2.get(s2[R],0) + 1
 
-            if hashmap1 == hashmap2 :
+            
+
+            while R-L+1 > len(s1):
+                cs2[s2[L]] -= 1
+                if cs2[s2[L]] == 0:
+                    cs2.pop(s2[L])
+                L += 1
+
+            if cs1 == cs2:
                 return True
+            
 
         return False
 
+            
 
 
-
-
-                
         
